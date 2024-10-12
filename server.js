065f71951fae40739787d6d641ca3b04a9adc684
ben7sys <ben@7sys.de>
2024-10-12 22:48:44 +0200

    node server.js

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 3001;
+
+// Serve static files from the current directory
+app.use(express.static(__dirname));
+
+// Route for the main page
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
