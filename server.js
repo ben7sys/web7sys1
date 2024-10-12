05a3cb70c05e12c1379d029c0a37795be000e9af
ben7sys <ben@7sys.de>
2024-10-13 01:02:30 +0200

    hero section

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3001;
 
+// Fügen Sie diese Zeile am Anfang der Datei hinzu
+const compression = require('compression');
+
+// Fügen Sie diese Zeile vor app.use(express.static(__dirname)); hinzu
+app.use(compression());
+
 // Serve static files from the current directory
 app.use(express.static(__dirname));
 
